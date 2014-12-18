ba2547c327d4b3a7fa64682609b256c716fa3649
LetiIS <buthuz777@mail.ru>
2014-12-18 23:17:51 +0300

    LETIISDESKTOP
    Запуск главного фремя

---
+++ LETIISDESKTOP.java
@@ -0,0 +1,21 @@
+package pack;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author abelash
+ */
+public class LETIISDESKTOP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+    
+}
